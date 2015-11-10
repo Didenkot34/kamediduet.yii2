9fae4bd19969922241ca6ffd594f9f8af6e32fcf
Yuriy Didenko <didenkot34@gmail.com>
2015-11-11 00:26:20 +0200

    clear

--- modules/tooltip/views/default/index.php
+++
@@ -1,9 +0,0 @@
-<div class="dummy dummy-menu dummy-menu-icons">
-    <ul>
-        <li><a class="tooltip" href="#"><i class="fa fa-fw fa-car"></i><span class="tooltip-content">You can easily navigate the city by car.</span></a></li>
-        <li><a class="tooltip" href="#"><i class="fa fa-fw fa-bicycle"></i><span class="tooltip-content">Get some fresh air and ride a slick bike.</span></a></li>
-        <li><a class="tooltip" href="#"><i class="fa fa-fw fa-plane"></i><span class="tooltip-content">Be über-super fancy and take the train.</a></li>
-        <li><a class="tooltip" href="#"><i class="fa fa-fw fa-bus"></i><span class="tooltip-content">Come on, ride the big blue bus with us.</span></a></li>
-        <li><a class="tooltip" href="#"><i class="fa fa-fw fa-fighter-jet"></i><span class="tooltip-content">Why not? You'll be there in mach time.</span></a></li>
-    </ul>
-</div>
