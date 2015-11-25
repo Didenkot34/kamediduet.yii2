b54139eff6bc6284096a26e70d762f53b31055c7
Yuriy Didenko <didenkot34@gmail.com>
2015-11-25 14:26:08 +0200

    added style to navbar-brand

--- modules/home/views/layouts/index.php
+++ modules/home/views/layouts/index.php
@@ -60,7 +60,7 @@ AppAssetIndex::register($this);
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Jules & Verne</a>
+                <a class="navbar-brand" href="/">Jules & Verne</a>
             </div>
             <div class="collapse navbar-collapse" id="custom-collapse">
                 <ul class="nav navbar-nav navbar-right">
